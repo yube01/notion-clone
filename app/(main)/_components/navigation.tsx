0ce16f9fb38d6440d552212c0cff619d919900a0
yube01 <yubraja008@gmail.com>
2023-11-07 17:13:17 +0545

    trash option added

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -12,6 +12,7 @@ import { api } from '@/convex/_generated/api';
 import { toast } from 'sonner';
 import { DocumentList } from './document-list';
 import { Popover, PopoverContent, PopoverTrigger } from '@/components/ui/popover';
+import { TrashBox } from './trash-box';
 
 export const Navigation = () => {
 
@@ -188,7 +189,7 @@ export const Navigation = () => {
         <PopoverContent
          side={isMobile ? "bottom" : "right"}
          className='p-0 w-72 '>
-            <p>Trash box</p>
+            <TrashBox/>
         </PopoverContent>
       </Popover>
     </div>
